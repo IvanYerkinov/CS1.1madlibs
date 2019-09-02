3060c8c4327679130e5393244a712b51a0ac0238
Ivan Yerkinov <ivan@Ivans-MacBook-Pro.local>
2019-09-02 16:13:48 -0700

    Added Input Functions, Madlib, and Swaping function

--- madlibs.py
+++ madlibs.py
@@ -12,6 +12,9 @@ class wordContainer:
         word.append(self.wordType.pop(0))
         return word
 
+    def getSize():
+        return len(listofWords)
+
 
 wC = wordContainer()
 
@@ -22,3 +25,14 @@ def inputWord(wordT, aWT, amount):
         word = word.split(" ")[0]
 
         wordContainer.addWord(word, aWT)
+
+
+madlibs = "Yesterday, <p> and I went to the park. On our way to the <a> park, we saw a <a> <n> on a bike. We also saw big <a> balloons tied to a <n>. Once we got tot he <a> park, the sky turned <a>. It started to <v> and <v>. <p> and I <v> all the way home. Tomorrow we will try to go to the <a> park again and hope it doesn't <v>."
+
+
+def swapWord(word):
+    looper = word.getSize()
+
+    for i in range(looper):
+        sw = word.getWord()
+        madlibs.replace(sw[1], sw[0], 1)
